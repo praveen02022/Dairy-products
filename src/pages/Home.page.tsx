ddb816d819cb5815a098830456538ffe0858004c
praveenkumar0023 <apraveenkumar1245@gmail.com>
2024-02-17 20:49:07 +0530

    sidebar

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,28 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-
-export function HomePage() {
+import { AppShell } from "@mantine/core";
+import React from "react";
+import Sidebar from '@/components/sidebar/sidebar';
+interface Props {
+  children: React.ReactNode;
+}
+export function HomePage(props:Props) {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <AppShell>
+        {
+          <div>
+            <Sidebar />
+          </div>
+        }
+        {}
+        <div>{props.children}</div>
+      </AppShell>
     </>
   );
 }
+
+interface Props {
+  children: React.ReactNode;
+}
+
